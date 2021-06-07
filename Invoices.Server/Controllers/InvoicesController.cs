1f64d0c6a425ac45586ac833499f87661d43f970
Wiktor Napierała <wiktor.napierala@outlook.com>
2021-06-08 00:30:24 +0200

    Initial commit

---
+++ Invoices.Server/Controllers/InvoicesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invoices.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await Task.FromResult("Hello from server");
+        }
+    }
+}

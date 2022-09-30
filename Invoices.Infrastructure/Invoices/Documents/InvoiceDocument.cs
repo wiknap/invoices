edcf422ac30553d1397db0824d3e49207de0e0ae
Wiktor Napierała <wiktor.napierala@outlook.com>
2022-09-30 23:20:20 +0200

    Footer/Header change

--- Invoices.Infrastructure/Invoices/Documents/InvoiceDocument.cs
+++ Invoices.Infrastructure/Invoices/Documents/InvoiceDocument.cs
@@ -32,15 +32,7 @@ public class InvoiceDocument : IDocument
         });
     }
 
-    private void ComposeHeader(IContainer container)
-    {
-        container.Text(text =>
-        {
-            text.DefaultTextStyle(s => s.FontSize(6));
-            text.Line("If undeliverable, please return to:");
-            text.Line($"{invoiceData.Seller.Name} - {invoiceData.Seller.GetFirstAddressLine()} - {invoiceData.Seller.GetSecondAddressLine()}");
-        });
-    }
+    private void ComposeHeader(IContainer container) => container.Element(ComposeUndeliverableInfo);
 
     private void ComposeContent(IContainer container)
     {
@@ -48,32 +40,23 @@ public class InvoiceDocument : IDocument
             .Grid(grid =>
             {
                 grid.Columns(2);
-                grid.Item().Text(text =>
-                {
-                    text.Line("Line");
-                });
-                grid.Item().AlignRight().Text(text =>
-                {
-                    text.Line("Line");
-                });
-                grid.Item(2).AlignRight().Text(text =>
-                {
-                    text.Line("Line");
-                });
-                grid.Item(2).Text(text =>
-                {
-                    text.Line("Line");
-                });
+                grid.Item().Text(text => { text.Line("Line"); });
+                grid.Item().AlignRight().Text(text => { text.Line("Line"); });
+                grid.Item(2).AlignRight().Text(text => { text.Line("Line"); });
+                grid.Item(2).Text(text => { text.Line("Line"); });
             });
     }
 
-    private void ComposeFooter(IContainer container)
+    private void ComposeFooter(IContainer container) => container.Element(ComposeUndeliverableInfo);
+
+    private void ComposeUndeliverableInfo(IContainer container)
     {
         container.Text(text =>
         {
             text.DefaultTextStyle(s => s.FontSize(6));
             text.Line("If undeliverable, please return to:");
-            text.Line("Wiktor Napierała - Grochowska 291/13 - 03-842 Warszawa - Poland ");
+            text.Line(
+                $"{invoiceData.Seller.Name} - {invoiceData.Seller.GetFirstAddressLine()} - {invoiceData.Seller.GetSecondAddressLine()}");
         });
     }
 }
